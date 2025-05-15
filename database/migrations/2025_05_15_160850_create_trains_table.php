d1b1c5c52e798cab1d479e5257df1ec8a3652ed4
GiuntaGiovanni <jhonniest741@gmail.com>
2025-05-15 18:43:46 +0200

    [feat] create migration to create table

--- database/migrations/2025_05_15_160850_create_trains_table.php
+++ database/migrations/2025_05_15_160850_create_trains_table.php
@@ -12,13 +12,23 @@ return new class extends Migration
     {
         Schema::create('trains', function (Blueprint $table) {
             $table->id();
+            $table->date('date');
+            $table->string('company', 200);
+            $table->string('train_type', 50);
+            $table->string('departure_station', 60);
+            $table->string('arrival_station', 60);
+            $table->time('departure_time');
+            $table->time('arrival_time')->nullable(); // può non essere noto
+            $table->string('train_number', 5);
+            $table->boolean('is_on_time')->nullable(); // se non si sa ancora
+            $table->unsignedTinyInteger('delay_minutes')->nullable();
+            $table->boolean('is_cancelled')->default(false);
             $table->timestamps();
         });
     }
 
-    /**
-     * Reverse the migrations.
-     */
+
+
     public function down(): void
     {
         Schema::dropIfExists('trains');
